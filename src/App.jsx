8e08100eb2f703c65d460b92b92ab7eb3f6ccef0
BridgetKas <bridgetnamugga@gmail.com>
2024-08-07 16:59:10 +0900

    Added todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState('')
 
   return (
     <div className='todoContainer'>
       <h1 className='todo'> Todo List</h1>
       <hr/>
       <div className='inputContainer'>
-        <input type='text' placeholder='add item...' className='input'/>
+        <input type='text' placeholder='add item...' value={input} className='input' onChange={(e) => setInput(e.target.value)}/>
         <div className='btnContainer'>
           <button className='edit'>Edit</button>
-          <button className='delete'>Delete</button>
+          <button className='delete' onClick={() =>setInput('')}>Delete</button>
         </div>
       </div>
       <div className='addContainer'>
