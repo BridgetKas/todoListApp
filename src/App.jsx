8af4c047936391bb3fad7446d35c8429c8ba86fa
BridgetKas <bridgetnamugga@gmail.com>
2024-08-16 19:38:43 +0900

    Added todoList app

--- src/App.jsx
+++ src/App.jsx
@@ -3,95 +3,151 @@ import './App.css'
 import Modal from './component/modal/modal';
 
 function App() {
-  const [todoTask , settodoTask] = useState('')
-  const [todos,setTodos] = useState([])
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [editTodoTask, setEditTodoTask] = useState('')
-  const [activeIndex, setActiveIndex] = useState(-1)
+  const[todoTask,setTodoTask] = useState('')
+  const[addTodo, setaddTodo] = useState([])
+  const [isModalOpen,setIsModalOpen] = useState(false)
+  const [editTodo,setEditTodo] = useState('')
+  const [activeIndex,setActiveIndex] = useState(-1)
+  const [completedTodos, setCompletedTodos] = useState([])
 
-  const openModal = (index) => {
-    const task = todos.find((task,taskIndex) => taskIndex === index)
-    setModalOpen(true);
-    setActiveIndex(index)
-    setEditTodoTask(task.title)
+  function addTodoTask(){
+    setaddTodo(
+      [
+        ...addTodo,
+        {
+          title:todoTask,
+          completed:false
+        }
+      ]
+    )
+    setTodoTask('')
+  }
+
+  function deleteTodo(index,name) {
+    if(name === 'notCompleted'){
+      const newTodo = addTodo.filter((item,todoIndex) => index !== todoIndex)
+      setaddTodo(newTodo)
+    }else {
+      const newTodo = completedTodos.filter((item,todoIndex) => index !== todoIndex)
+      setCompletedTodos(newTodo)
+
+    }
    
   }
-    
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        title:todoTask,
-        completedTask:false
-      }
-    ])
-    settodoTask('')
-  }
 
-  function deleteTodo(index) {
-    const todoTasks = todos.filter((item,i) => index !== i)
-    setTodos(todoTasks)
-  }
-
-  function checkedTask(index) {
-    const todoTaskChecked = todos.map((item,indexTask) =>{
-      if(index === indexTask) {
-        return {
-          ...item,
-          completedTask:!item.completedTask
+  function markAsCompleted(index,arrayName) {
+    if(arrayName === 'uncompleteTask') {
+      const completedTasks = addTodo.map((item,todoIndex) => {
+        if(index === todoIndex){
+          return {
+            ...item,
+            completed:!item.completed
+          }
+        }else{
+          return item
         }
-      }else {
+      })
+      const uncompletedTasks = completedTasks.filter((item) => !item.completed)
+      setaddTodo(uncompletedTasks)
+      const completedTask = completedTasks.find((item) => item.completed === true)
+      setCompletedTodos(
+        [
+          ...completedTodos,
+          completedTask
+        ]
+      )
+    }else{
+      const completedTasks = completedTodos.map((item,todoIndex) => {
+        if(index === todoIndex){
+          return {
+            ...item,
+            completed:!item.completed
+          }
+        }else{
+          return item
+        }
+      })
+      const unCompletedTask = completedTasks.find((item) => item.completed === false)
+      setaddTodo([...addTodo,unCompletedTask])
+      const checkedTask = completedTasks.filter(item => item.completed === true)
+      console.log(checkedTask)
+      setCompletedTodos(checkedTask)
+    }
+  }
+
+  function openModal(index) {
+    setIsModalOpen(true)
+    setActiveIndex(index)
+    const todoItem = addTodo.find((item,todoIndex) => index === todoIndex)
+    setEditTodo(todoItem.title)
+  }
+  
+  function saveTodo(){
+    const savedTodo = addTodo.map((item,todoIndex) => {
+      if(todoIndex === activeIndex){
+        return{
+          ...item,
+          title:editTodo
+        }
+      }else{
         return item
       }
     })
-    setTodos(todoTaskChecked)
-  
+    setaddTodo(savedTodo)
+    setIsModalOpen(false)
   }
-  
-  function saveTodo() {
-    const saveChanges = todos.map((task,index) =>{
-      if(index === activeIndex) {
-        return {
-          ...task,
-          title:editTodoTask
-        }
-      }else {
-        return task;
-      }
-    })
-    setTodos(saveChanges)
-    setEditTodoTask('')
-    setModalOpen(false)
-  }
+
+
   return (
     <div className='todoContainer'>
-      <h1 className='todo'> Todo List</h1>
+      <h1 className='Task'> Todo List</h1>
       <hr/>
       <div className='mainInput'>
-        <input type='text' placeholder='add item...' value={todoTask} className='input' onChange={(e) => settodoTask(e.target.value)}/>
+        <input type='text' placeholder='add item...' value={todoTask} className='input' onChange={(e) => setTodoTask(e.target.value)}/>
       </div>
       <div className='addContainer'>
-        <button className='add' type='button' onClick={addTodo}>ADD</button>
+        <button className='add' type='button' onClick={addTodoTask}>ADD</button>
       </div>
       <ul className='container'>
         {
-          todos.map((item ,index) =>(
+          addTodo.map((item ,index) =>(
             <div key={index} className='completedContainer inputContainer'>
-              <input type='checkbox' onChange={()=> checkedTask(index)} checked={item.completedTask}/>
-              <li className='completed'> {item.title}</li>
+              <input type='checkbox' onChange={()=> markAsCompleted(index,'uncompleteTask')} checked={item.completed}/>
+              <li className='completed' > {item.title}</li>
               <div className='btnContainer'>
                 <button className='edit' onClick={() =>openModal(index)}>Edit</button>
-                <button className='delete' onClick={() => deleteTodo(index)}>Delete</button>
+                <button className='delete' onClick={() => deleteTodo(index,'notCompleted')}>Delete</button>
               </div>
             </div>
             
           ))
         }
       </ul>
+      <ul className='container'>
+        {
+          completedTodos.map((item,index) =>(
+            <div key={index} className='completedContainer inputContainer' 
+              style={{ 
+              backgroundColor:item.completed ? 'green':'',
+              color:item.completed ? 'white':'',
+              textDecoration:item.completed ?'line-through' :'none'
+            }}>
+              <input type='checkbox' onChange={()=> markAsCompleted(index,'completeTask')} checked={item.completed}/>
+              <li className='completed' > {item.title}</li>
+              <div className='btnContainer'>
+                <button className='edit' onClick={() =>openModal(index)}>Edit</button>
+                <button className='delete' onClick={() => deleteTodo(index,'completed')}>Delete</button>
+              </div>
+            </div>
+            
+          ))
+        }
+
+      </ul>
       <div>
       <Modal show={isModalOpen}>
         <div className='modalContent'>
-          <input type='text' placeholder='add item...' value={editTodoTask} className='input' onChange={(e) => setEditTodoTask(e.target.value)}/>
+          <input type='text' placeholder='add item...' value={editTodo} className='input' onChange={(e) => setEditTodo(e.target.value)}/>
             <div className='modalContainer'>
               <button className='edit' onClick={saveTodo}>Save</button>
             </div>
